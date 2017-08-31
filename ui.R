8cdebec6db3d6e4e30814c099a7efae311620f88
stur600 <stur600@aucklanduni.ac.nz>
2017-09-01 11:28:16 +1200

    changed path directory for csv

--- ui.R
+++ ui.R
@@ -12,7 +12,7 @@ library(RCurl)
 library(networkD3)
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  Network_df_NumberedNodes <- read.csv(file = "C:/Users/stur600/Documents/Network_df_NumberedNodes.csv"),
+  Network_df_NumberedNodes <- read.csv(file = "Network_df_NumberedNodes.csv"),
   links = Network_df_NumberedNodes,
   
   #Manually create nodes dataframe 
