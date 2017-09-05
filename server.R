fab8669c76d3954bc3051403f6201160987b490b
stur600 <stur600@aucklanduni.ac.nz>
2017-09-05 16:06:20 +1200

    Added side panel with options for interacting with sankey

--- server.R
+++ server.R
@@ -15,63 +15,247 @@ shinyServer(function(input, output) {
    
   output$sankeyNetwork <- renderSankeyNetwork({
     
-    # Load data for network links
-    Network_df_NumberedNodes <- read.csv(file = "Users/stur600/Documents/Network_df_NumberedNodes.csv")
-    female_prop_links <- read.csv(file = "Users/stur600/Documents/female_prop_links.csv")
-    male_prop_links <- read.csv(file = "Users/stur600/Documents/male_prop_links.csv")
-    male_links <- read.csv(file = "Users/stur600/Documents/male_links.csv")
-    female_links <- read.csv(file = "Users/stur600/Documents/female_links.csv")
-    
-    ifelse(input$dataset == "Raw Counts", links = Network_df_NumberedNodes, 
-         ifelse(input$dataset == "Female Counts", links = female_links,
-              ifelse(input$dataset == "Male Counts", links = male_links,
-                     ifelse(input$dataset == "Female Proportion", links= female_prop_links,
-                            ifelse(input$dataset == "Male Proportion", links = male_prop_links, NULL)))))
-    
+    #if we don't want to include physics 160 students...
+    if(input$Physics160 == FALSE){ 
     #Manually create nodes dataframe 
-    nodes = data.frame("name" = c("GPE_Bottom", 
+    nodes = data.frame("name" = c("GPE_Top", 
                                   "GPE_Middle", 
-                                  "GPE_Top", 
+                                  "GPE_Bottom", 
                                   
-                                  "Physics120_Bottom", 
+                                  "Physics120_Top", 
                                   "Physics120_Middle", 
-                                  "Physics120_Top",
-                              
-                                  "Physics150_Bottom",
+                                  "Physics120_Bottom",
+                                  
+                                  "Physics150_Top",
                                   "Physics150_Middle",
-                                  "Physics150_Top", 
-                                  
-                                  # "Physics160_Bottom", #Comment out to exclude Physics160 students
-                                  # "Physics160_Middle",
-                                  # "Physics160_Top",
-                                  
-                                  "S2GPA_Bottom",
+                                  "Physics150_Bottom", 
+                                 
+                                  "S2GPA_Top",
                                   "S2GPA_Middle",
-                                  "S2GPA_Top", 
-                                      
-                                  "S3GPA_Bottom",
+                                  "S2GPA_Bottom", 
+                                  
+                                  "S3GPA_Top",
                                   "S3GPA_Middle",
-                                  "S3GPA_Top"),
-                           
+                                  "S3GPA_Bottom"),
+                       
                        "Source_Group" = c("1","1","1",
                                           "2",'2',"2",
                                           '3',"3","3",
                                           "4","4",'4',
                                           "5","5",'5')) #Source_group allows us to manually assign group to nodes.
-        
-        
-        
+    
+    #Load data for network links (These files do not include physics160 students)
+    Network_df_NumberedNodes <- read.csv(file = "Network_df_NumberedNodes.csv")
+    female_prop_links <- read.csv(file = "female_prop_links.csv")
+    male_prop_links <- read.csv(file = "male_prop_links.csv")
+    male_links <- read.csv(file = "male_links.csv")
+    female_links <- read.csv(file = "female_links.csv")
+    prop_by_gender_F <- read.csv(file = "prop_by_gender_F.csv")
+    prop_by_gender_M <- read.csv(file = "prop_by_gender_M.csv")
+    
+    #make sankey network using dataset specified by user. 
+    if(input$dataset == "Raw Counts"){
+      links = Network_df_NumberedNodes
       sankeyNetwork(Links = links, Nodes = nodes,
-                    Source = "Source", Target = "Target",
-                    Value = "n", NodeID = "name",
-                    NodeGroup = "Source_Group",
-                    LinkGroup = "gender", 
-                    units = "students",
-                    nodePadding = 75,
-                    fontSize= 12, nodeWidth = 20,
-                    
-                    colourScale = "d3.scaleOrdinal().range(['#ef8a62','#67a9cf','#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])") #First two colours are the links, last 5 are the nodes.
-      })
+                  Source = "Source", Target = "Target",
+                  Value = "n", NodeID = "name",
+                  NodeGroup = "Source_Group",
+                  LinkGroup = "gender", 
+                  units = "students",
+                  nodePadding = 75,
+                  fontSize= 12, nodeWidth = 20,
+                  colourScale = "d3.scaleOrdinal().range(['#ef8a62','#67a9cf','#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}   #First two colours are the links, last 5 are the nodes.
+    
+      else if(input$dataset == "Female Counts"){
+        links = female_links
+        sankeyNetwork(Links = links, Nodes = nodes,
+                      Source = "Source", Target = "Target",
+                      Value = "n", NodeID = "name",
+                      NodeGroup = "Source_Group",
+                      units = "students",
+                      nodePadding = 75,
+                      fontSize= 12, nodeWidth = 20,
+                      colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+    
+        else if(input$dataset == "Male Counts"){
+          links = male_links
+          sankeyNetwork(Links = links, Nodes = nodes,
+                        Source = "Source", Target = "Target",
+                        Value = "n", NodeID = "name",
+                        NodeGroup = "Source_Group",
+                        units = "students",
+                        nodePadding = 75,
+                        fontSize= 12, nodeWidth = 20,
+                        colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+    
+          else if(input$dataset == "Female Percentage"){
+            links= female_prop_links
+            sankeyNetwork(Links = links, Nodes = nodes,
+                          Source = "Source", Target = "Target",
+                          Value = "n", NodeID = "name",
+                          NodeGroup = "Source_Group",
+                          units = "percent female",
+                          nodePadding = 75,
+                          fontSize= 12, nodeWidth = 20,
+                          colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+    
+            else if(input$dataset == "Male Percentage"){
+              links = male_prop_links
+              sankeyNetwork(Links = links, Nodes = nodes,
+                            Source = "Source", Target = "Target",
+                            Value = "n", NodeID = "name",
+                            NodeGroup = "Source_Group",
+                            units = "percent male",
+                            nodePadding = 75,
+                            fontSize= 12, nodeWidth = 20,
+                            colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+    
+              else if(input$dataset == "Female Percentage of Source"){
+                links = prop_by_gender_F
+                sankeyNetwork(Links = links, Nodes = nodes,
+                              Source = "Source", Target = "Target",
+                              Value = "n", NodeID = "name",
+                              NodeGroup = "Source_Group",
+                              units = "percent",
+                              nodePadding = 75,
+                              fontSize= 12, nodeWidth = 20,
+                              colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+    
+                else if(input$dataset == "Male Percentage of Source"){
+                  links = prop_by_gender_M
+                  sankeyNetwork(Links = links, Nodes = nodes,
+                                Source = "Source", Target = "Target",
+                                Value = "n", NodeID = "name",
+                                NodeGroup = "Source_Group",
+                                units = "percent",
+                                nodePadding = 75,
+                                fontSize= 12, nodeWidth = 20,
+                                colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+                          
+    #if we want to include physics for life science students... 
+    } else { 
+      nodes = data.frame("name" = c("GPE_Top", 
+                                    "GPE_Middle", 
+                                    "GPE_Bottom", 
+                                    
+                                    "Physics160_Top", #include Physics160 tertiles in our nodes
+                                    "Physics160_Middle",
+                                    "Physics160_Bottom",
+                                    
+                                    "Physics120_Top", 
+                                    "Physics120_Middle", 
+                                    "Physics120_Bottom",
+                                    
+                                    "Physics150_Top",
+                                    "Physics150_Middle",
+                                    "Physics150_Bottom", 
+                                    
+                                    
+                                    "S2GPA_Top",
+                                    "S2GPA_Middle",
+                                    "S2GPA_Bottom", 
+                                    
+                                    "S3GPA_Top",
+                                    "S3GPA_Middle",
+                                    "S3GPA_Bottom"),
+                         
+                         "Source_Group" = c("1","1","1",
+                                            "2",'2',"2",
+                                            '3',"3","3",
+                                            "4","4",'4',
+                                            "5","5",'5',
+                                            "6", "6", "6")) #Source_group allows us to manually assign group to nodes.
+      
+      #Load data for network links (these files include physics 160 students)
+      Network_df_NumberedNodes <- read.csv(file = "Network_df_NumberedNodes_160.csv")
+      female_prop_links <- read.csv(file = "female_prop_links_160.csv")
+      male_prop_links <- read.csv(file = "male_prop_links_160.csv")
+      male_links <- read.csv(file = "male_links_160.csv")
+      female_links <- read.csv(file = "female_links_160.csv")
+      prop_by_gender_F <- read.csv(file = "prop_by_gender_F_160.csv")
+      prop_by_gender_M <- read.csv(file = "prop_by_gender_M_160.csv")
+      
+      # create sankey network based on input specified by user.
+      if(input$dataset == "Raw Counts"){
+        links = Network_df_NumberedNodes
+        sankeyNetwork(Links = links, Nodes = nodes,
+                      Source = "Source", Target = "Target",
+                      Value = "n", NodeID = "name",
+                      NodeGroup = "Source_Group",
+                      LinkGroup = "gender", 
+                      units = "students",
+                      nodePadding = 75,
+                      fontSize= 12, nodeWidth = 20,
+                      colourScale = "d3.scaleOrdinal().range(['#ef8a62','#67a9cf','#f7f7f7', '#a1dab4','#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}   #First two colours are the links, last 5 are the nodes.
+      
+      else if(input$dataset == "Female Counts"){
+        links = female_links
+        sankeyNetwork(Links = links, Nodes = nodes,
+                      Source = "Source", Target = "Target",
+                      Value = "n", NodeID = "name",
+                      NodeGroup = "Source_Group",
+                      units = "students",
+                      nodePadding = 75,
+                      fontSize= 12, nodeWidth = 20,
+                      colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+      
+      else if(input$dataset == "Male Counts"){
+        links = male_links
+        sankeyNetwork(Links = links, Nodes = nodes,
+                      Source = "Source", Target = "Target",
+                      Value = "n", NodeID = "name",
+                      NodeGroup = "Source_Group",
+                      units = "students",
+                      nodePadding = 75,
+                      fontSize= 12, nodeWidth = 20,
+                      colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+      
+      else if(input$dataset == "Female Percentage"){
+        links= female_prop_links
+        sankeyNetwork(Links = links, Nodes = nodes,
+                      Source = "Source", Target = "Target",
+                      Value = "n", NodeID = "name",
+                      NodeGroup = "Source_Group",
+                      units = "percent female",
+                      nodePadding = 75,
+                      fontSize= 12, nodeWidth = 20,
+                      colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+      
+      else if(input$dataset == "Male Percentage"){
+        links = male_prop_links
+        sankeyNetwork(Links = links, Nodes = nodes,
+                      Source = "Source", Target = "Target",
+                      Value = "n", NodeID = "name",
+                      NodeGroup = "Source_Group",
+                      units = "percent male",
+                      nodePadding = 75,
+                      fontSize= 12, nodeWidth = 20,
+                      colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+      
+      else if(input$dataset == "Female Percentage of Source"){
+        links = prop_by_gender_F
+        sankeyNetwork(Links = links, Nodes = nodes,
+                      Source = "Source", Target = "Target",
+                      Value = "n", NodeID = "name",
+                      NodeGroup = "Source_Group",
+                      units = "percent",
+                      nodePadding = 75,
+                      fontSize= 12, nodeWidth = 20,
+                      colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+      
+      else if(input$dataset == "Male Percentage of Source"){
+        links = prop_by_gender_M
+        sankeyNetwork(Links = links, Nodes = nodes,
+                      Source = "Source", Target = "Target",
+                      Value = "n", NodeID = "name",
+                      NodeGroup = "Source_Group",
+                      units = "percent",
+                      nodePadding = 75,
+                      fontSize= 12, nodeWidth = 20,
+                      colourScale = "d3.scaleOrdinal().range(['#f7f7f7', '#a1dab4', '#a1dab4', '#31a354', '#006d2c'])")}
+      
+    }
     })
+})
     
 
--- ui.R
+++ ui.R
@@ -11,65 +11,66 @@ library(shiny)
 library(RCurl)
 library(networkD3)
 
-Network_df_NumberedNodes <- read.csv(file = "Users/stur600/Documents/Network_df_NumberedNodes.csv")
-female_prop_links <- read.csv(file = "Users/stur600/Documents/female_prop_links.csv")
-male_prop_links <- read.csv(file = "Users/stur600/Documents/male_prop_links.csv")
-male_links <- read.csv(file = "Users/stur600/Documents/male_links.csv")
-female_links <- read.csv(file = "Users/stur600/Documents/female_links.csv")
-
-
-    
-#Manually create nodes dataframe 
-nodes = data.frame("name" = c("GPE_Bottom", 
-                              "GPE_Middle", 
-                              "GPE_Top", 
-                              
-                              "Physics120_Bottom", 
-                              "Physics120_Middle", 
-                              "Physics120_Top",
-                              
-                              "Physics150_Bottom",
-                              "Physics150_Middle",
-                              "Physics150_Top", 
-                              
-                              # "Physics160_Bottom", #Comment out to exclude Physics160 students
-                              # "Physics160_Middle",
-                              # "Physics160_Top",
-                              
-                              "S2GPA_Bottom",
-                              "S2GPA_Middle",
-                              "S2GPA_Top", 
-                              
-                              "S3GPA_Bottom",
-                              "S3GPA_Middle",
-                              "S3GPA_Top"),
-                   
-                   "Source_Group" = c("1","1","1",
-                                      "2",'2',"2",
-                                      '3',"3","3",
-                                      "4","4",'4',
-                                      "5","5",'5')) #Source_group allows us to manually assign group to nodes.
-
 
 # Define UI for application that draws a sankey
 shinyUI(fluidPage(
   
+ 
   
+  Network_df_NumberedNodes <- read.csv(file = "Network_df_NumberedNodes.csv"),
+  female_prop_links <- read.csv(file = "female_prop_links.csv"),
+  male_prop_links <- read.csv(file = "male_prop_links.csv"),
+  male_links <- read.csv(file = "male_links.csv"),
+  female_links <- read.csv(file = "female_links.csv"),
+  
+  
+  
+  #Manually create nodes dataframe 
+  nodes = data.frame("name" = c("GPE_Top", 
+                                "GPE_Middle", 
+                                "GPE_Bottom", 
+                                
+                                "Physics120_Top", 
+                                "Physics120_Middle", 
+                                "Physics120_Bottom",
+                                
+                                "Physics150_Top",
+                                "Physics150_Middle",
+                                "Physics150_Bottom", 
+                                
+                                # "Physics160_Top", #Comment out to exclude Physics160 students
+                                # "Physics160_Middle",
+                                # "Physics160_Bottom",
+                                
+                                "S2GPA_Top",
+                                "S2GPA_Middle",
+                                "S2GPA_Bottom", 
+                                
+                                "S3GPA_Top",
+                                "S3GPA_Middle",
+                                "S3GPA_Bottom"),
+                     
+                     "Source_Group" = c("1","1","1",
+                                        "2",'2',"2",
+                                        '3',"3","3",
+                                        "4","4",'4',
+                                        "5","5",'5')), #Source_group allows us to manually assign group to nodes.
   
   # Application title
   titlePanel("Sankey Network of Grade Progression Through Physics"),
   
   sidebarLayout(
-    
+
     sidebarPanel(
-      selectInput(inputID = "dataset", 
-                  label = "Choose a dataset:", 
-                  choices = c("Raw Counts", "Female Counts", "Male Counts", "Female Proportion", "Male Proportion"))
-      )
-    ),
+      selectInput(inputId = "dataset",
+                  label = "Choose a dataset:",
+                  choices = c("Raw Counts", "Female Counts", "Male Counts", "Female Percentage of Total", "Male Percentage of Total", "Female Percentage of Source", "Male Percentage of Source")),
+      
+      checkboxInput(inputId = "Physics160", 
+                    label = "Include Physics 160 (Physics for Life Sciences)?")),
   
   mainPanel(
       sankeyNetworkOutput("sankeyNetwork")
-    )
+    ))
   
 ))
